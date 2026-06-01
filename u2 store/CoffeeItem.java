7b0c2c0ef45f934afbe62be37ae5968f7de97586
s166788 <s166788@scusd.net>
2026-06-01 09:43:45 -0700

    a

---
+++ u2 store/CoffeeItem.java
@@ -0,0 +1,29 @@
+public class CoffeeItem {
+  private String name;
+  private double price;
+
+  public CoffeeItem(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String toString() {
+    return name + ": $" + price;
+  }
+}
